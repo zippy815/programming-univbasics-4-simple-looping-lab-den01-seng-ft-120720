5ef9748938354c22f139495fa8c26c6389caba83
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-04 23:54:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,8 @@
 # Write your methods here
-def loop_message_five_times(greeting)
-  greeting = "Hello world"
+def loop_message_five_times(message)
+  count = 0
+  while count < 5
+  do 
+    puts message
+    count += 1
 end
