39739373e29b0bd293898ddd51c56f95e42a6faa
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-04 23:57:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,9 +6,9 @@ def loop_message_five_times(message)
     count += 1
   end
 end
-def loop_message_n_times(message)
+def loop_message_n_times(message, number)
   count = 0
-  while count < 5 do 
+  while count < number 
     puts message
     count += 1
   end
